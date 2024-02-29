9a7d9115a76b0af398af73ca6b82e3419f1dd11d
AdarshSantoria <adarshsantoria@gmail.com>
2024-02-29 19:56:18 +0530

    Initial commit

--- src/app/Garements/api/route.ts
+++ src/app/Garements/api/route.ts
@@ -3,21 +3,26 @@ import Data from "../data";
 import connectDB from "@/lib/mongoose";
 
 export async function POST(req: any){
-    const {cart} = await req.json();
+    const { cart } = await req.json();
 
     try {
         await connectDB();
-        await Data.create({
-            id: cart[0].id,
-            name: cart[0].name,
-            image: cart[0].image
-        })
-        return NextResponse.json({msg: ["Message Sent"]})
+        
+        // Loop through each item in the cart array
+        for (const item of cart) {
+            await Data.create({
+                id: item.id,
+                name: item.name,
+                image: item.image
+            });
+        }
+
+        return NextResponse.json({ msg: ["Message Sent"] });
     }
     catch (error) {
         console.log(error);
     }
-    console.log(cart);
 
-    return NextResponse.json({msg: ["hi!"]})
+    console.log(cart);
+    return NextResponse.json({ msg: ["hi!"] });
 }
